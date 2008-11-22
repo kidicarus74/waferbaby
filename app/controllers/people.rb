56be0973a76472e35894da8cce2022bd8a26d596
Daniel Bogan <waferbaby@scornful.local>
2008-11-22 21:34:02 +1100

    Removed the edit, update and this_is_me methods.

--- app/controllers/people.rb
+++ app/controllers/people.rb
@@ -19,30 +19,12 @@ class People < Application
                 render
         end
 
-        def edit
-                only_provides :html
-                @person = Person.first(:username => params[:username])
-                raise NotFound unless this_is_me(@person)
-
-                render
-        end
-
         def create
                 @person = Person.new(params[:person])
                 if @person.save
                         redirect url(:person, @person)
                 else
                         render :new
-                end
-        end
-
-        def update
-                @person = Person.get(params[:username])
-                raise NotFound unless this_is_me(@person)
-                if @person.update_attributes(params[:person]) || !@person.dirty?
-                        redirect url(:person, @person)
-                else
-                        raise BadRequest
                 end
         end
 
@@ -56,8 +38,4 @@ class People < Application
                 end
         end
         
-        def this_is_me(person)
-                person && person == current_person
-        end
-
 end
