3a50359206e219d621ad34d0aadbb6beb39b1396
Daniel Bogan <waferbaby@scornful.local>
2008-11-23 07:18:54 +1100

    Removed the link_to_post_categories method.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,19 +5,5 @@
 
 module Merb
         module PostsHelper
-                def link_to_post_categories(post)
-                        post.categories.collect do |category|
-                                if category.slug == params[:category]
-                                        category.name
-                                else
-                                        link_to(category.name,
-                                                url(:posts, :category => category.slug),
-                                                :title  => "View all posts filed under '#{category.name}'.",
-                                                :class  => 'category',
-                                                :rel    => 'tag'
-                                        )                                
-                                end
-                        end.join(', ')
-                end
         end
 end
