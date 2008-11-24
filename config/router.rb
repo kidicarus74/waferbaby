05a03fea917571c204857d8071683b2a37551cb2
Daniel Bogan <waferbaby@scornful.local>
2008-11-24 20:24:17 +1100

    Updated to use identify for Person, Post and Scrawl resources.

--- config/router.rb
+++ config/router.rb
@@ -1,23 +1,25 @@
 Merb.logger.info("Compiling routes...")
-Merb::Router.prepare do |r|
-        r.match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
-        r.match('/login').to(:controller => 'sessions', :action => 'new').name(:login)
-        r.match('/logout').to(:controller => 'sessions', :action => 'destroy').name(:logout)
-        r.match('/signup').to(:controller => 'people', :action => 'new').name(:signup)
+Merb::Router.prepare do
+        match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
+        match('/login').to(:controller => 'sessions', :action => 'new').name(:login)
+        match('/logout').to(:controller => 'sessions', :action => 'destroy').name(:logout)
+        match('/signup').to(:controller => 'people', :action => 'new').name(:signup)
         
-        r.resources(:people, :keys => ['username']) do |people|
-        end
+	identify Person => :username do
+        	resources(:people)
+	end
+
+	identify Post => [:created_year, :created_month, :created_day, :slug] do
+		resources(:posts)
+	end
+
+	identify Scrawl => [:created_year, :created_month, :created_day] do
+        	resources(:scrawls)
+	end
         
-        r.resources(:posts, :keys => ['created_year', 'created_month', 'created_day', 'slug']) do |posts|
-                posts.resources(:comments)
-        end
+        match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]', :created_day => '[4]')
+        match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]')
+        match(%r'/([a-z]+)/([0-9]{4,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]')
         
-        r.resources(:wallscrawl)
-        
-        r.match(%r'/([a-z]+)/categories/(.+)').to(:controller => '[1]', :action => 'index_by_category', :category => '[2]')
-        r.match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]', :created_day => '[4]').name(:scrawls_by_day)
-        r.match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]').name(:scrawls_by_month)
-        r.match(%r'/([a-z]+)/([0-9]{4,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]').name(:scrawls_by_year)
-        
-        r.match('/').to(:controller => 'posts', :action => 'index').name(:posts)
+        match('/').to(:controller => 'posts', :action => 'index').name(:default)
 end
