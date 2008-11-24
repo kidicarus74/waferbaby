376fea8922bde67a924622d1540a1c7367dcac52
Daniel Bogan <waferbaby@scornful.local>
2008-11-24 20:22:50 +1100

    Moved the dependencies to the top of the file.

--- config/init.rb
+++ config/init.rb
@@ -1,31 +1,28 @@
-$KCODE = 'UTF8'
+dependency 'rdiscount'
+dependency 'uuid'
 
-Gem.clear_paths
-Gem.path.unshift(Merb.root / "gems")
+dependency 'merb-action-args'
+dependency 'merb-assets'
+dependency 'merb-helpers'
+dependency 'merb-param-protection'
+ 
+dependency 'dm-core'
+dependency 'dm-aggregates'
+dependency 'dm-migrations'
+dependency 'dm-timestamps'
+dependency 'dm-validations'
+
+require 'lib/timestamp_extensions'
 
 use_orm                         :datamapper
 use_test                        :rspec
 use_template_engine             :haml
 
-Merb::BootLoader.before_app_loads do
-        Merb.add_mime_type(:atom, :to_atom, %w[application/atom+xml])
-end
-
-dependency      'dm-aggregates'
-dependency      'dm-timestamps'
-dependency      'dm-validations'
-dependency      'md5'
-dependency      'merb-parts'
-dependency      'merb-action-args'
-dependency      'merb-assets'
-dependency      'merb-haml'
-dependency      'merb_helpers'
-dependency      'merb-mailer'
-dependency      'rdiscount'
-dependency      'uuid'
-dependency      'lib/timestamp_extensions'
-
 Merb::Config.use do |c|
         c[:session_secret_key]  = '1703bf9efcbb694541de2ce7c48b9406da71ac00'
         c[:session_store]       = 'datamapper'
+end
+
+Merb::BootLoader.before_app_loads do
+        Merb.add_mime_type(:atom, :to_atom, %w[application/atom+xml])
 end
