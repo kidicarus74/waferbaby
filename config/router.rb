a1f29e603f9509ce282bc9f92732723ddee2b154
Daniel Bogan <github.com@waferbaby.com>
2008-11-25 20:09:20 +1100

    Moved the index_by_date matches further up the chain.

--- config/router.rb
+++ config/router.rb
@@ -4,6 +4,10 @@ Merb::Router.prepare do
         match('/login').to(:controller => 'sessions', :action => 'new').name(:login)
         match('/logout').to(:controller => 'sessions', :action => 'destroy').name(:logout)
         match('/signup').to(:controller => 'people', :action => 'new').name(:signup)
+
+        match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]', :created_day => '[4]')
+        match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]')
+        match(%r'/([a-z]+)/([0-9]{4,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]')
         
 	identify Person => :username do
         	resources(:people)
@@ -16,10 +20,6 @@ Merb::Router.prepare do
 	identify Scrawl => [:created_year, :created_month, :created_day] do
         	resources(:scrawls)
 	end
-        
-        match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]', :created_day => '[4]')
-        match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]')
-        match(%r'/([a-z]+)/([0-9]{4,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]')
         
         match('/').to(:controller => 'posts', :action => 'index').name(:default)
 end
