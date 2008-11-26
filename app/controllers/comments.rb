6d0aca90d88504914ef595b85199e3df87097768
Daniel Bogan <github.com@waferbaby.com>
2008-11-26 21:43:22 +1100

    Initial import.

---
+++ app/controllers/comments.rb
@@ -0,0 +1,63 @@
+class Comments < Application
+	def index
+		@comments = Comment.all
+		display @comments
+	end
+
+	def show
+		@comment = Comment.get(params[:id])
+		raise NotFound unless @comment
+		display @comment
+	end
+
+	def new
+		only_provides :html
+		
+		@comment = Comment.new
+		@post = Post.first(:slug => params[:slug])
+		
+		render
+	end
+
+	def edit
+		only_provides :html
+		@comment = Comment.get(params[:id])
+		raise NotFound unless @comment
+		render
+	end
+
+	def create
+		@post = Post.first(:slug => params[:slug])
+		
+		@comment = Comment.new(params[:comment])
+		@comment.person = current_person
+		
+		@post.comments << @comment
+		
+		if @post.save
+			redirect resource(@post) + "/#from-#{@comment.person.username}-at-#{@comment.created_time}"
+		else
+			render :new
+		end
+	end
+
+	def update
+		@comment = Comment.get(params[:id])
+		raise NotFound unless @comment
+		if @comment.update_attributes(params[:comment]) || !@comment.dirty?
+			redirect resource(:comments)
+		else
+			raise BadRequest
+		end
+	end
+
+	def destroy
+		@comment = Comment.get(params[:id])
+		raise NotFound unless @comment
+		if @comment.destroy
+			redirect resource(:comments)
+		else
+			raise BadRequest
+		end
+	end
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+class Comment
+        include DataMapper::Resource
+        include DataMapper::Timestamp
+        
+        property :id,           Integer, :serial => true
+        property :uuid,         String, :length => 36
+        property :contents,     Text
+        property :created_at,   DateTime
+        property :updated_at,   DateTime        
+        
+        belongs_to :person
+        belongs_to :post
+end
