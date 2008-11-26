c6cbab7f83810015bc6f3fd76928c7c76761d3d6
Daniel Bogan <github.com@waferbaby.com>
2008-11-26 22:08:37 +1100

    Updated create to check the comment is valid before adding it to the post.

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -34,7 +34,7 @@ class Comments < Application
 		
 		@post.comments << @comment
 		
-		if @post.save
+		if @comment.valid? && @post.save
 			redirect resource(@post) + "/#from-#{@comment.person.username}-at-#{@comment.created_time}"
 		else
 			render :new
