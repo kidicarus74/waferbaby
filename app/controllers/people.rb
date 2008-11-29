da4cee555c347596ee0e503ce71a41bda21b75aa
Daniel Bogan <github.com@waferbaby.com>
2008-11-29 13:52:57 +1100

    Added the index_by_letter_or_number method.

--- app/controllers/people.rb
+++ app/controllers/people.rb
@@ -7,8 +7,24 @@ class People < Application
         # provides :xml, :yaml, :js
 
         def index
-                @people = Person.all(:order => [:created_at.desc])
+                @people = Person.all(:order => [:created_at.desc], :limit => 20)
                 display @people
+        end
+
+	def index_by_letter_or_number(character)
+		@people = Person.all(:username.like => "#{character}%", :order => [:username.asc])
+		
+		display @people, :index
+	end
+	
+	def index_by_date(created_year, created_month = nil, created_day = nil)
+                created_month = '__' if created_month == nil
+                created_day   = '__' if created_day   == nil
+                
+                date    = "#{created_year}-#{created_month}-#{created_day}%"
+                @people  = Person.all(:created_at.like => date, :order => [:created_at.desc])
+                
+                display @people, :index
         end
 
         def show
