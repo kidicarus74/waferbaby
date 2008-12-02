1e76fc7dd851a6553d027ab9ae3368abd5f4e6d9
Daniel Bogan <github.com@waferbaby.com>
2008-12-02 22:57:40 +1100

    Updated the new method to set a session message as needed.

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -7,12 +7,18 @@ class Sessions < Application
 
         def new
                 if request.post?
-                        self.current_person = Person.authenticate(params[:username], params[:password])
-                        if logged_in?
-                                url = session[:return_to] || "/"
-                                session[:return_to] = nil
-                                redirect(url)
-                        end
+			if params[:username].blank? || params[:password].blank?
+				session[:message] = "We need both your username and password!"
+			else
+	                        self.current_person = Person.authenticate(params[:username], params[:password])
+	                        if logged_in?
+	                                url = session[:return_to] || "/"
+	                                session[:return_to] = nil
+	                                redirect(url)
+				else
+					session[:message] = "Your username or password seems to be wrong. Try again?"
+	                        end
+			end
                 end
                 
                 render
