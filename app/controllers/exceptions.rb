01b29a550b5c3144ca4b287efacdbce349f7532f
Daniel Bogan <github.com@waferbaby.com>
2008-12-13 21:23:10 +1100

    Fixed up tabs.

--- app/controllers/exceptions.rb
+++ app/controllers/exceptions.rb
@@ -4,11 +4,11 @@
 #
 
 class Exceptions < Application
-        def not_found
-                render :format => :html
-        end
+	def not_found
+		render :format => :html
+	end
 
-        def not_acceptable
-                render :format => :html
-        end
+	def not_acceptable
+		render :format => :html
+	end
 end
