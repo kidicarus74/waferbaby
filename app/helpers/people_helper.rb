6fcbcb5a4ae87393c386cd5125700073fac534fc
Daniel Bogan <github.com@waferbaby.com>
2008-12-22 23:38:59 +1100

    Added the name_for_person and link_to_person methods.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -5,5 +5,13 @@
 
 module Merb
         module PeopleHelper
+		
+		def name_for_person(person)
+			h(person.display_name.blank? ? person.username : person.display_name)
+		end
+		
+		def link_to_person(person)
+			link_to(name_for_person(person), url(:person, person), :title => "View more about '#{h(person.username)}'.")
+		end
         end
 end
