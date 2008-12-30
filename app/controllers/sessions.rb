5b4d574266d2a434c99b17c0020c8226c8f69d25
Daniel Bogan <github.com@waferbaby.com>
2008-12-30 22:55:09 +1100

    Updated to support the remember_me cookie.

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -11,6 +11,11 @@ class Sessions < Application
 			else
 	                        self.current_person = Person.authenticate(params[:username], params[:password])
 	                        if logged_in?
+					if params[:remember_me]
+						self.current_person.remember_me
+						cookies.set_cookie(:payload_of_love, self.current_person.remember_token, :path => '/', :expires => 14.days.from_now, :domain => '.waferbaby.com')
+					end
+		
 	                                url = session[:return_to] || "/"
 	                                session[:return_to] = nil
 	                                redirect(url)
@@ -24,6 +29,9 @@ class Sessions < Application
         end
 
         def destroy
+		self.current_person.forget_me if logged_in?
+		cookies.delete(:payload_of_love)
+		
                 session.clear!
                 redirect("/") 
         end
