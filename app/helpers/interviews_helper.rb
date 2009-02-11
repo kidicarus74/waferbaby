22d94f32dba10ebd88e81892796d763ae3603e0b
Daniel Bogan <github.com@waferbaby.com>
2009-02-11 21:08:22 +1100

    Added the icon_for_interview method.

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -1,5 +1,13 @@
-module Merb
-  module InterviewsHelper
+#
+#	interviews_helper.rb
+#	Copyright (c) 2008-2009 Daniel Bogan. http://waferbaby.com/
+#
 
-  end
-end # Merb
+module Merb
+	module InterviewsHelper
+		
+		def icon_for_interview(interview, width = 40, height = 40)
+			image_tag("setup/#{interview.slug}.thumb.jpg", :width => width, :height => height, :alt => h(interview.title), :class => 'icon')
+		end
+	end
+end
