f399fd58c3129d42db3e5ec51044d28eb5ba9dcc
Daniel Bogan <github.com@waferbaby.com>
2009-02-18 21:03:44 +1100

    Initial import.

---
+++ app/models/entry.rb
@@ -0,0 +1,28 @@
+#
+#	comment.rb
+#	Copyright (c) 2008-2009 Daniel Bogan. http://waferbaby.com/
+#
+
+class Entry
+
+	include DataMapper::Resource
+        include DataMapper::Timestamp
+        
+        property :id,           Serial
+        property :uuid,         String, :length => 36
+	property :type,		Discriminator
+        property :contents,     Text
+        property :created_at,   DateTime
+        property :updated_at,   DateTime        
+        
+        belongs_to :person
+
+	is_paginated
+
+	validates_present       :contents
+	
+	before :save do
+		self.uuid = UUID.generate
+	end
+	
+end
