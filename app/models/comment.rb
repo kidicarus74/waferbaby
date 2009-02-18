30ba61aac3ea70c469d6f0c91151bdfceec3217c
Daniel Bogan <github.com@waferbaby.com>
2009-02-18 21:07:45 +1100

    Became a subclass of the Entry class.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,26 +1,10 @@
 #
-#	comment.rb
+#	entry.rb
 #	Copyright (c) 2008-2009 Daniel Bogan. http://waferbaby.com/
 #
 
-class Comment
-        include DataMapper::Resource
-        include DataMapper::Timestamp
-        
-        property :id,           Serial
-        property :uuid,         String, :length => 36
-        property :contents,     Text
-        property :created_at,   DateTime
-        property :updated_at,   DateTime        
-        
-        belongs_to :person
-        has n,			:posts, :through => Resource
-
-	is_paginated
-
-	validates_present       :contents
+class Comment < Entry
 	
-	before :save do
-		self.uuid = UUID.generate
-	end
+        has n,	:posts, :through => Resource
+
 end
--- app/models/scrawl.rb
+++ app/models/scrawl.rb
@@ -3,23 +3,5 @@
 #	Copyright (c) 2008-2009 Daniel Bogan. http://waferbaby.com/
 #
 
-class Scrawl
-        include DataMapper::Resource
-        include DataMapper::Timestamp
-        
-        property :id,           Serial
-        property :uuid,         String, :length => 36
-        property :contents,     Text
-        property :created_at,   DateTime
-        property :updated_at,   DateTime
-        
-	is_paginated
-
-        belongs_to :person
-        
-        validates_present       :contents
-        
-        before :save do
-                self.uuid = UUID.generate
-        end
+class Scrawl < Entry
 end
