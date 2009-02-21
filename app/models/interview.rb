9c55b8e808506894cd1c35106b68bc97990bdb42
Daniel Bogan <github.com@waferbaby.com>
2009-02-22 02:01:39 +1100

    Cleaned up the text alignment for the id property. Clearly, this is a big deal.

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -6,8 +6,9 @@
 class Interview
 
 	include DataMapper::Resource
+	include DataMapper::Timestamp
 
-	property :id, Serial
+	property :id, 		Serial
         property :uuid,         String, :length => 36
         property :slug,         String
         property :title,        String
