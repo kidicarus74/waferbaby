1e36069798b6b335c144cbbd634a146643b86f36
Daniel Bogan <github.com@waferbaby.com>
2009-02-24 23:24:28 +1100

    Added the link_to_scrawl and url_for_scrawl methods.

--- app/helpers/scrawls_helper.rb
+++ app/helpers/scrawls_helper.rb
@@ -1,5 +1,13 @@
 module Merb
-  module ScrawlsHelper
+	module ScrawlsHelper
 
-  end
-end # Merb
+		def link_to_scrawl(scrawl, title)
+			link_to(title, url_for_scrawl(scrawl), :title => "Permalink for this scrawl.")  
+		end
+		
+		def url_for_scrawl(scrawl)
+			date_url_for(scrawl, 'wallscrawl') + "#at-#{scrawl.created_time}"
+		end
+
+	end
+end
