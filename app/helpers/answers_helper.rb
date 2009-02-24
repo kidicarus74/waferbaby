ff9ab33ddcc247f4c68652e7721320b9ccf2f3e6
Daniel Bogan <github.com@waferbaby.com>
2009-02-24 23:23:25 +1100

    Added the link_to_answer and url_for_answer.

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,5 +1,13 @@
 module Merb
-  module AnswersHelper
+	module AnswersHelper
 
-  end
-end # Merb
+		def link_to_answer(answer, title)
+			link_to(title, url_for_answer(answer), :title => "Permalink for this answer.")
+		end
+		
+		def url_for_answer(answer)
+			url(:brainstorm, answer.brainstorm[0]) + "#answer-at-#{answer.created_time}"
+		end
+
+	end
+end
