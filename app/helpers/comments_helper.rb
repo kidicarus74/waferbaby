f134447e6530c7153bbd06dc421838267fcf60da
Daniel Bogan <github.com@waferbaby.com>
2009-02-24 23:23:59 +1100

    Added the link_to_comment and url_for_comment methods.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,13 @@
 module Merb
-  module CommentsHelper
-
-  end
-end # Merb
+	module CommentsHelper
+		
+		def link_to_comment(comment, title)
+			link_to(title, url_for_comment(comment), :title => "Permalink for this comment.")
+		end
+		
+		def url_for_comment(comment)
+			url(:post, comment.post[0]) + "#comment-at-#{comment.created_time}"
+		end
+		
+	end
+end
